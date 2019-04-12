6dbfaf7c736eccf63c050b48b93b1b29a52d6f90
robert4213 <573679310@qq.com>
2019-04-11 21:47:07 -0700

    code update

---
+++ code/src/Main.java
@@ -0,0 +1,17 @@
+import processing.core.*;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("Main");
+	}
+	
+	public void setting() {
+		size(1080,1920);
+	}
+	
+	public void draw() {
+		rect(0,0,12,12);
+	}
+}
