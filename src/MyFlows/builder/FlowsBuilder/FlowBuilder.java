919c970943b48a7883be8d97583840e82aff2d85
yijun zhang <zhangyijun0518@gmail.com>
2019-04-26 16:05:16 -0700

    Observer pattern for total amount of flows display in  header

--- src/MyFlows/builder/FlowsBuilder/FlowBuilder.java
+++ src/MyFlows/builder/FlowsBuilder/FlowBuilder.java
@@ -4,5 +4,16 @@
  */
 public class FlowBuilder implements IFlowSubject
 {
+    private IFlowObserver ob;
+    private String incomeTol;
+    private String outcomeTol;
     
+    public void attach(IFlowObserver observer){
+        ob = observer;
+    };
+  
+    public void notifyObservers(){
+        ob.flowSumUpdate(false, incomeTol);
+        ob.flowSumUpdate(true, outcomeTol);
+    };
 }
