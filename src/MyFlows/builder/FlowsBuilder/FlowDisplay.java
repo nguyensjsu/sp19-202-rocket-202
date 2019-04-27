06e2fb35e90b96896e26bf529a2e5edf02db3f8c
yijun zhang <zhangyijun0518@gmail.com>
2019-04-26 18:55:24 -0700

    define  FlowItem as the minimum unit for display combined flowItemDisplay and FlowDisplay

--- src/MyFlows/builder/FlowsBuilder/FlowDisplay.java
+++
@@ -1,6 +0,0 @@
-
-
-public interface FlowDisplay
-{
-    boolean flow();
-}
--- src/MyFlows/builder/FlowsBuilder/FlowItem.java
+++ src/MyFlows/builder/FlowsBuilder/FlowItem.java
@@ -1,9 +1,47 @@
 
-/**
- * Each Flow Item interface
- */
-public interface FlowItem
+
+public class FlowItem implements IFlowItem, IDisplayComponent, ITouchEventHandler
 {
-    String typeName();
-    float value();
+    String[] flowDetail;
+    String icopath;
+    private ITouchEventHandler nextHandler;
+    
+    public FlowItem(String detail){
+        flowDetail = detail.split(",");
+    }
+    
+    public String typeName(){
+        return flowDetail[1];
+    };
+    
+    public String imgPath(){
+        return "img/" + typeName() +".png";
+    };
+    
+    public float value(){
+        return flowDetail[2];
+    };
+    
+    public boolean outcome(){
+        if (flowDetail[0].equals("TRUE")){
+            return true ;
+        } else{
+            return false;
+        }
+    };
+    
+    public void touch(){
+        //trigger delete cvs item; 
+        if (nextHandler != null){
+           nextHandler.touch(); 
+        }
+    };
+
+    public void setNext(ITouchEventHandler next){
+       nextHandler = next;
+    };
+   
+    public void display(){};
+
+    public void addSubComponent(IDisplayComponent c){};
 }
